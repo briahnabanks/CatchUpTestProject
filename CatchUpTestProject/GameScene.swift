f8fca6eb4d1bd9a22933e14a8de345e325cc0694
Joseph Brinker <joey.brinker805@outlook.com>
2025-01-30 08:38:01 -0500

    Swipe Functionality

--- CatchUpTestProject/GameScene.swift
+++ CatchUpTestProject/GameScene.swift
@@ -10,75 +10,75 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var car : SKSpriteNode!
+    var columnPositions = [CGFloat]()
+    var initialTouchPosition: CGPoint?
+    var isSwipeActionCommitted = false
+
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        columnPositions = [
+            -175, 0, 175
+        ]
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //Initialize column positions
+        car = self.childNode(withName: "car") as? SKSpriteNode
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        car.position = CGPoint(x: columnPositions[0], y: -500)
+
     }
     
-    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first {
+            initialTouchPosition = touch.location(in: self)
+            isSwipeActionCommitted = false
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let initialTouchPosition = initialTouchPosition, !isSwipeActionCommitted else { return }
+        if let touch = touches.first {
+            let currentTouchPosition = touch.location(in: self)
+            let movement = currentTouchPosition.x - initialTouchPosition.x
+
+            if abs(movement) > 200 { // Threshold to detect a swipe
+                if movement > 0 {
+                    print("Swiped right")
+                    if car.position.x < columnPositions[2] {
+                        car.position.x += 175
+                    }
+                } else {
+                    print("Swiped left")
+                    if car.position.x > columnPositions[0] {
+                        car.position.x -= 175
+                    }
+                }
+                // Reset initial position to prevent continuous detection
+                self.initialTouchPosition = currentTouchPosition
+                
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        initialTouchPosition = nil // Reset when touch ends
+        isSwipeActionCommitted = false
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
