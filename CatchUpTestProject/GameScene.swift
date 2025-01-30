27b880eb414c21e243c086744ec37cd9cc38b7dc
Briahna Banks <briahnabanks@gmail.com>
2025-01-30 08:35:18 -0500

    Scrolling background + Drag gesture

--- CatchUpTestProject/GameScene.swift
+++ CatchUpTestProject/GameScene.swift
@@ -10,79 +10,76 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var scrollLayer: SKNode!
+    var car: SKSpriteNode!
+    var roadDraft: SKNode!
+    
+    let  scrollSpeed: CGFloat = 100
+    
+    let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
     
     override func didMove(to view: SKView) {
+        /* Setup your scene here */
+        scrollLayer = self.childNode(withName: "scrollLayer")
+        car = self.childNode(withName: "car") as? SKSpriteNode
+       
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //*gesture recognizer*
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
+        //        let swipeRight : UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipedRight))
+        //
+        //
+        //        swipeRight.direction = .right
+        //        view.addGestureRecognizer(swipeRight)
+        //
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    @objc func swipedRight(sender: UISwipeGestureRecognizer) { print("Object has been swiped")}
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { print("swipe detected")
+        for touch in touches {
+            let location = touch.location(in:self)
+            car.position.x = location.x
         }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
+        /* Called when a touch begins */
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        /* Called before each frame is rendered */
+        scrollWorld()
+        
     }
+    
+    func scrollWorld(){
+        scrollLayer.position.y -= scrollSpeed * CGFloat(fixedDelta)
+        
+        /* Loop through scroll layer nodes */
+        for roadDraft in scrollLayer.children as! [SKSpriteNode] { 
+
+          /* Get ground node position, convert node position to scene space */
+            let roadDraftPosition = scrollLayer.convert(roadDraft.position, to: self)
+
+          /* Check if ground sprite has left the scene */
+            if roadDraftPosition.y <= -roadDraft.size.width / 1 {
+
+              /* Reposition ground sprite to the second starting position */
+                let newPosition = CGPoint(x: (self.size.width / 2) + roadDraft.size.width, y: roadDraftPosition.y);
+
+              /* Convert new node position back to scroll layer space */
+                roadDraft.position = self.convert(newPosition, to: scrollLayer); print("scroll")
+          }
+        }
+    }
+    
 }
+        
+      
+
